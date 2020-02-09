d09f9d038410306f3c12e0dbd2b0acce701b7877
Nelson Andres Tique Morales <60830527+NelsonM9@users.noreply.github.com>
2020-02-09 08:57:36 -0500

    Delete ejercicio3.4.py

--- ejercicio3.4.py
+++
@@ -1,6 +0,0 @@
-n1 = int(input("ingrese el primer numero"))
-n2 = int(input("ingrese el primer numero"))
-n3 = int(input("ingrese el primer numero"))
-if n1>n2 and n2>n1 and n3>n1:
-    print("el numero ingresado que es")
-    pass
